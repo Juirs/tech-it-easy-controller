9b227a2da7b67fdccb8dd9d1fb1e525917b321bd
Ahmed Bestun <zeroaiko@live.com>
2025-05-26 12:32:29 +0200

    start

---
+++ src/main/java/com/tech_it_easy/controller/TechItEasyControllerApplication.java
@@ -0,0 +1,13 @@
+package com.tech_it_easy.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItEasyControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechItEasyControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech_it_easy/controller/TechItEasyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_it_easy.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
